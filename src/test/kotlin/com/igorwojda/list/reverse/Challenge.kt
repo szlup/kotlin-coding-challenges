8180590ac576a672abc1fcc289203f85632525ad
szlup <whitney.zac@gmail.com>
2025-08-09 23:28:51 -0400

    first pass at solution

--- src/test/kotlin/com/igorwojda/list/reverse/Challenge.kt
+++ src/test/kotlin/com/igorwojda/list/reverse/Challenge.kt
@@ -3,9 +3,10 @@ package com.igorwojda.list.reverse
 import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.Test
 
+
+
 private fun reverse(list: List<String>): List<String> {
     if (list.isEmpty()) return listOf()
-
     else {
         val result: MutableList<String> = mutableListOf()
         for (i in (list.size - 1) downTo 0) {
--- src/test/kotlin/com/igorwojda/string/ispalindrome/basic/Challenge.kt
+++ src/test/kotlin/com/igorwojda/string/ispalindrome/basic/Challenge.kt
@@ -4,7 +4,8 @@ import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.Test
 
 private fun isPalindrome(str: String): Boolean {
-    TODO("Add your solution here")
+    return if (str.isEmpty()) true
+    else str == str.reversed()
 }
 
 private class Test {
