5276237bfaa2bef411e8a96879eba6a7cf7db42a
szlup <whitney.zac@gmail.com>
2025-08-10 10:57:03 -0400

    working solution

--- src/test/kotlin/com/igorwojda/string/ispalindrome/permutation/Challenge.kt
+++ src/test/kotlin/com/igorwojda/string/ispalindrome/permutation/Challenge.kt
@@ -3,8 +3,22 @@ package com.igorwojda.string.ispalindrome.permutation
 import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.Test
 
-private fun isPermutationPalindrome(str: String): Boolean {
-    TODO("Add your solution here")
+private fun isPermutationPalindrome(str: String, oddFound: Boolean = false): Boolean {
+    if (str.isEmpty()) return true
+    else {
+        val charCount = str.count {it == str[0]}
+        val newStr = str.replace(str[0].toString(), "")
+        if (charCount % 2 == 0) {
+            return isPermutationPalindrome(newStr, oddFound = oddFound)
+        } else {
+            if (newStr.isEmpty()) {
+                return if (oddFound) false
+                else true
+            } else {
+                return isPermutationPalindrome(newStr, oddFound = true)
+            }
+        }
+    }
 }
 
 private class Test {
