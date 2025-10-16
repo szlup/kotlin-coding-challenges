c5482335d4de33e3a1e168ff39fe365e9a1dfb21
zwhitney <zac@name-coach.com>
2025-10-16 17:24:38 -0400

    LRUCache working solution

--- src/test/kotlin/com/igorwojda/cache/lru/Challenge.kt
+++ src/test/kotlin/com/igorwojda/cache/lru/Challenge.kt
@@ -1,5 +1,7 @@
 package com.igorwojda.cache.lru
 
+import java.util.LinkedList
+
 interface LRUCache<K: Any, V: Any> {
     val size: Int
 
@@ -8,13 +10,25 @@ interface LRUCache<K: Any, V: Any> {
 }
 
 class LRUCacheImpl<K: Any, V: Any>(private val capacity: Int) : LRUCache<K, V> {
-    override val size: Int get() = TODO("Add your solution here")
+    override val size: Int get() = dict.size
+
+    private val dict: HashMap<K, V> = hashMapOf()
+    private val accessHistory: LinkedList<K> = LinkedList()
+
 
     override fun get(key: K): V? {
-        TODO("Add your solution here")
+        val result = dict[key]
+        if(accessHistory.lastOrNull() != key) accessHistory.add(key)
+        if(accessHistory.firstOrNull() == key) accessHistory.removeFirst()
+        return result
     }
 
     override fun put(key: K, value: V) {
-        TODO("Add your solution here")
+        dict[key] = value
+        accessHistory.add(key)
+        if (this.size > capacity) {
+            dict.remove(accessHistory.removeFirst())
+        }
+
     }
 }
