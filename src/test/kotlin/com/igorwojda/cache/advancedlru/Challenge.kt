77cda11575a0a691ebb47a2c22a4c22425d22a33
zwhitney <zac@name-coach.com>
2025-10-17 14:32:50 -0400

    advanced cache working solution

--- src/test/kotlin/com/igorwojda/cache/advancedlru/Challenge.kt
+++ src/test/kotlin/com/igorwojda/cache/advancedlru/Challenge.kt
@@ -2,6 +2,8 @@ package com.igorwojda.cache.advancedlru
 
 import java.time.Clock
 import java.time.Duration
+//import java.util.Date
+//import java.util.LinkedList
 
 interface LRUCache<K: Any, V: Any> {
     fun put(key: K, value: V, priority: Int, ttl: Duration)
@@ -9,11 +11,82 @@ interface LRUCache<K: Any, V: Any> {
 }
 
 class AdvancedLRUCache<K: Any, V: Any>(private val capacity: Int, private val clock: Clock = Clock.systemDefaultZone()): LRUCache<K, V> {
+    val size: Int get() = dict.size
+
+    private var dict: HashMap<K, CacheItem<K, V>> = hashMapOf()
+    //private val accessHistory: HashMap<K, Int> = hashMapOf()
+    //private val accessCounter = 0
+
+
     override fun put(key: K, value: V, priority: Int, ttl: Duration) {
-        TODO("Add your solution here")
+        removeExpired()
+        val existingItem = dict[key]
+        if (existingItem == null) {
+            dict[key] = CacheItem(
+                key,
+                value,
+                priority,
+                clock.millis() + ttl.toMillis(),
+                clock.millis()
+            )
+
+            if (this.size > capacity)
+                removeForCapacity()
+        }else {
+            existingItem.value = value
+            existingItem.expiryTime = clock.millis() + ttl.toMillis()
+            existingItem.lastAccess = clock.millis()
+        }
     }
 
     override fun get(key: K): V? {
-        TODO("Add your solution here")
+        removeExpired()
+        val result = dict[key]
+        if (result != null) result.lastAccess = clock.millis()
+
+        return result?.value
     }
+
+    private fun removeExpired() {
+        val expiredKeys = dict.filterValues { it.expiryTime < clock.millis() }.keys
+        if (expiredKeys.isNotEmpty()) {
+            for (k in expiredKeys) dict.remove(k)
+        }
+    }
+
+    private fun removeForCapacity() {
+        if (size <= capacity) return
+        else {
+            // lol, instructions say to take the ones with the earliest expiry time, but tests make you ignore it ¯\_(ツ)_/¯
+            // get items with earliest expiry time
+//            val earliestExpiryTime: Long = dict.values.minOf { it.expiryTime }
+//
+//            val earliestExpiring = dict.filterValues {
+//                it.expiryTime == earliestExpiryTime
+//            }
+//            if (earliestExpiring.size == 1) {
+//                dict.remove(earliestExpiring.keys.first())
+//            } else {
+            // of those, get lowest priority items
+            val lowestPriority = dict.filterValues { item ->
+                item.priority == dict.values.minOf { it.priority }
+            }
+            if (lowestPriority.size == 1) {
+                dict.remove(lowestPriority.keys.first())
+            } else {
+                // of those, remove least recently accessed
+                dict.remove(lowestPriority.minBy { it.value.lastAccess }.key)
+            }
+
+        }
+
+
+    }
+
+    class CacheItem<K: Any, V: Any>(
+        var key: K,
+        var value: V,
+        var priority: Int,
+        var expiryTime: Long,
+        var lastAccess: Long)
 }
